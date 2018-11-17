3d239f8c6ea387d902f0288180390682327731d2
David Kent <dk657@cornell.edu>
2018-11-17 11:30:34 -0500

    compactify the mse plotting

--- data_analysis/ground_truth/MSE_analysis.R
+++ data_analysis/ground_truth/MSE_analysis.R
@@ -1,138 +1,50 @@
 library(ggplot2)
+#library(cowplot)
 library(dplyr)
+library(tidyr)
 
-ex1 <- read.csv("ex1_mse.csv",header=F)
-names(ex1) <- c("meth","task","mse")
-ex1_sum <- ex1 %>%
-  group_by(meth,task) %>%
-  summarize(mean=mean(mse),
-         se=sd(mse)/n())
-ex1 %>%
-  ggplot(aes(x=mse)) +
-  geom_histogram(aes(color=meth,fill=meth)) +
-  geom_point(data=ex1_sum,
-             aes(x=mean,y=0)) +
-  geom_vline(data=ex1_sum,
-             aes(xintercept=mean)) +
-  geom_errorbarh(data=ex1_sum,
-             aes(x=NULL,xmin=mean-2*se,xmax=mean+2*se,y=0)) +
-  facet_grid(meth~task)
+#theme_set(theme_bw())
 
-ex2 <- read.csv("ex2_mse.csv",header=F)
-names(ex2) <- c("meth","task","mse")
-ex2_sum <- ex2 %>%
-  group_by(meth,task) %>%
-  summarize(mean=mean(mse),
-         se=sd(mse)/n())
-ex2 %>%
-  ggplot(aes(x=mse)) +
-  geom_histogram(aes(color=meth,fill=meth)) +
-  geom_point(data=ex2_sum,
-             aes(x=mean,y=0)) +
-  geom_vline(data=ex2_sum,
-             aes(xintercept=mean)) +
-  geom_errorbarh(data=ex2_sum,
-             aes(x=NULL,xmin=mean-2*se,xmax=mean+2*se,y=0)) +
-  facet_grid(meth~task)
+tasks <- "example1.csv"
+mse_unif <- "ex1_mse.csv"
+mse_int <- "ex1_interval_mse.csv"
 
-ex3 <- read.csv("ex3_mse.csv",header=F)
-names(ex3) <- c("meth","task","mse")
-ex3_sum <- ex3 %>%
-  group_by(meth,task) %>%
-  summarize(mean=mean(mse),
-         se=sd(mse)/n())
-ex3 %>%
-  ggplot(aes(x=mse)) +
-  geom_histogram(aes(color=meth,fill=meth)) +
-  geom_point(data=ex3_sum,
-             aes(x=mean,y=0)) +
-  geom_vline(data=ex3_sum,
-             aes(xintercept=mean)) +
-  geom_errorbarh(data=ex3_sum,
-             aes(x=NULL,xmin=mean-2*se,xmax=mean+2*se,y=0)) +
-  facet_grid(meth~task)
+mse_plots <- function(tasks,mse_unif,mse_int) {
+  ex1_tasks <- read.csv(tasks,header=F) %>%
+    gather(task,y,2:3)
+  
+  task_plot <- ggplot(ex1_tasks,aes(x=V1,y=y,color=task)) +
+    scale_color_discrete(guide=F) +
+    scale_y_continuous(labels=NULL,breaks=NULL) +
+    labs(x=NULL,y=NULL) +
+    theme_bw() +
+    geom_line()
+  
+  ex1_unif <- read.csv(mse_unif,header=F)
+  names(ex1_unif) <- c("meth","task","mse")
+  ex1_unif$train <- "uniform"
+  ex1_int <- read.csv(mse_int,header=F)
+  names(ex1_int) <- c("meth","task","mse")
+  ex1_int$train <- "interval"
+  ex1 <- rbind(ex1_unif,ex1_int) %>%
+    group_by(meth,task,train) %>%
+    summarize(mean=mean(mse),
+              se=sd(mse)/n())
+  ex1$meth <- relevel(ex1$meth,ref="Multi")
+  ex1$task <- as.factor(ex1$task)
+  
+  mse_plot <- ggplot(ex1,aes(x=mean,color=task)) +
+    geom_point(aes(x=mean,y=0)) +
+    geom_errorbarh(aes(xmin=mean-2*se,xmax=mean+2*se,y=0)) +
+    labs(x="MSE") +
+    scale_y_continuous(name=NULL,labels=NULL,breaks = NULL) +
+    scale_color_discrete(guide=F) +
+    theme_bw() +
+    facet_grid(meth~train)
+  list(cowplot::plot_grid(task_plot,mse_plot,nrow=2,rel_heights=c(1,2),align="b"))
+}
 
-ex4 <- read.csv("ex4_mse.csv",header=F)
-names(ex4) <- c("meth","task","mse")
-ex4_sum <- ex4 %>%
-  group_by(meth,task) %>%
-  summarize(mean=mean(mse),
-         se=sd(mse)/n())
-ex4 %>%
-  ggplot(aes(x=mse)) +
-  geom_histogram(aes(color=meth,fill=meth)) +
-  geom_point(data=ex4_sum,
-             aes(x=mean,y=0)) +
-  geom_vline(data=ex4_sum,
-             aes(xintercept=mean)) +
-  geom_errorbarh(data=ex4_sum,
-             aes(x=NULL,xmin=mean-2*se,xmax=mean+2*se,y=0)) +
-  facet_grid(meth~task)
-
-ex1_interval <- read.csv("ex1_interval_mse.csv",header=F)
-names(ex1_interval) <- c("meth","task","mse")
-ex1_interval_sum <- ex1_interval %>%
-  group_by(meth,task) %>%
-  summarize(mean=mean(mse),
-         se=sd(mse)/n())
-ex1_interval %>%
-  ggplot(aes(x=mse)) +
-  geom_histogram(aes(color=meth,fill=meth)) +
-  geom_point(data=ex1_interval_sum,
-             aes(x=mean,y=0)) +
-  geom_vline(data=ex1_interval_sum,
-             aes(xintercept=mean)) +
-  geom_errorbarh(data=ex1_interval_sum,
-             aes(x=NULL,xmin=mean-2*se,xmax=mean+2*se,y=0)) +
-  facet_grid(meth~task)
-
-ex2_interval <- read.csv("ex2_interval_mse.csv",header=F)
-names(ex2_interval) <- c("meth","task","mse")
-ex2_interval_sum <- ex2_interval %>%
-  group_by(meth,task) %>%
-  summarize(mean=mean(mse),
-         se=sd(mse)/n())
-ex2_interval %>%
-  ggplot(aes(x=mse)) +
-  geom_histogram(aes(color=meth,fill=meth)) +
-  geom_point(data=ex2_interval_sum,
-             aes(x=mean,y=0)) +
-  geom_vline(data=ex2_interval_sum,
-             aes(xintercept=mean)) +
-  geom_errorbarh(data=ex2_interval_sum,
-             aes(x=NULL,xmin=mean-2*se,xmax=mean+2*se,y=0)) +
-  facet_grid(meth~task)
-
-ex3_interval <- read.csv("ex3_interval_mse.csv",header=F)
-names(ex3_interval) <- c("meth","task","mse")
-ex3_interval_sum <- ex3_interval %>%
-  group_by(meth,task) %>%
-  summarize(mean=mean(mse),
-         se=sd(mse)/n())
-ex3_interval %>%
-  ggplot(aes(x=mse)) +
-  geom_histogram(aes(color=meth,fill=meth)) +
-  geom_point(data=ex3_interval_sum,
-             aes(x=mean,y=0)) +
-  geom_vline(data=ex3_interval_sum,
-             aes(xintercept=mean)) +
-  geom_errorbarh(data=ex3_interval_sum,
-             aes(x=NULL,xmin=mean-2*se,xmax=mean+2*se,y=0)) +
-  facet_grid(meth~task)
-
-ex4_interval <- read.csv("ex4_interval_mse.csv",header=F)
-names(ex4_interval) <- c("meth","task","mse")
-ex4_interval_sum <- ex4_interval %>%
-  group_by(meth,task) %>%
-  summarize(mean=mean(mse),
-         se=sd(mse)/n())
-ex4_interval %>%
-  ggplot(aes(x=mse)) +
-  geom_histogram(aes(color=meth,fill=meth)) +
-  geom_point(data=ex4_interval_sum,
-             aes(x=mean,y=0)) +
-  geom_vline(data=ex4_interval_sum,
-             aes(xintercept=mean)) +
-  geom_errorbarh(data=ex4_interval_sum,
-             aes(x=NULL,xmin=mean-2*se,xmax=mean+2*se,y=0)) +
-  facet_grid(meth~task)
+mse_plots("example1.csv","ex1_mse.csv","ex1_interval_mse.csv")
+mse_plots("example2.csv","ex2_mse.csv","ex2_interval_mse.csv")
+mse_plots("example3.csv","ex3_mse.csv","ex3_interval_mse.csv")
+mse_plots("example4.csv","ex4_mse.csv","ex4_interval_mse.csv")
